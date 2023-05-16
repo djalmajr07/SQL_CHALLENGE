5743ccbb8265a00b3e27abe66bc2c6d10a56fb4e
djr07 <djalmajr07@gmail.com>
2023-05-16 15:40:04 -0400

    solved business question 1 - 3

---
+++ Case 3 - Foodie Fi/B. Data Analysis Questions.sql
@@ -0,0 +1,58 @@
+-- 1. How many customers has Foodie-Fi ever had?
+-- 2. What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
+-- 3. What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name
+-- 4. What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
+-- 5. How many customers have churned straight after their initial free trial - what percentage is this rounded to the nearest whole number?
+-- 6. What is the number and percentage of customer plans after their initial free trial?
+-- 7. What is the customer count and percentage breakdown of all 5 plan_name values at 2020-12-31?
+-- 8. How many customers have upgraded to an annual plan in 2020?
+-- 9. How many days on average does it take for a customer to an annual plan from the day they join Foodie-Fi?
+-- 10. Can you further breakdown this average value into 30 day periods (i.e. 0-30 days, 31-60 days etc)
+-- 11. How many customers downgraded from a pro monthly to a basic monthly plan in 2020?
+
+
+-- 1. How many customers has Foodie-Fi ever had?
+
+SELECT 
+	COUNT(DISTINCT customer_id) AS total_customers
+FROM subscriptions s 
+
+-- 2. What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
+
+SELECT 
+	MONTH (start_date) AS trial_start,
+	YEAR  (start_date) AS trial_start,
+	COUNT(DISTINCT customer_id) AS total_customers_monthly
+FROM subscriptions s 
+INNER JOIN plans p USING(plan_id)
+WHERE plan_name = 'trial'
+GROUP BY 1,2;
+
+## both provide the same result
+
+SELECT DATE_FORMAT(start_date, '%Y-%m-01') AS month_start,
+       COUNT(*) AS count
+FROM subscriptions s 
+INNER JOIN plans p USING(plan_id)
+WHERE plan_name = 'trial'
+GROUP BY 1
+ORDER BY month_start;
+
+
+
+-- 3. What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name
+
+SELECT 
+	plan_name,
+	COUNT(customer_id) AS amoun_in_2021
+FROM subscriptions s 
+INNER JOIN plans p USING(plan_id)
+WHERE start_date >= '2021-01-01'
+GROUP BY 1
+
+
+-- 4. What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
+
+
+SELECT * FROM plans p ;
+SELECT * FROM subscriptions s 
